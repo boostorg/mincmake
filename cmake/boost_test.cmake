b38e3eff7d06bf28ec95cb2d74f3840f01f85007
Peter Dimov <pdimov@gmail.com>
2019-01-11 05:53:39 +0200

    Fix boost_test_jamfile

--- cmake/boost_test.cmake
+++ cmake/boost_test.cmake
@@ -83,24 +83,25 @@ function(boost_test_jamfile)
 
             string(REGEX MATCHALL "[^ ]+" ll ${line})
 
-            list(GET ll 0 e0)
+            if(ll)
+                list(GET ll 0 e0)
 
-            if(e0 IN_LIST types)
+                if(e0 IN_LIST types)
 
-                list(LENGTH ll lln)
+                    list(LENGTH ll lln)
 
-                if(lln GREATER 2)
+                    if(NOT lln EQUAL 2)
 
-                    message("Jamfile line ignored: ${line}")
+                        message("Jamfile line ignored: ${line}")
 
-                else()
+                    else()
 
-                    list(GET ll 1 e1)
-                    boost_test(PREFIX ${__PREFIX} TYPE ${e0} SOURCES ${e1} LIBRARIES ${__LIBRARIES})
+                        list(GET ll 1 e1)
+                        boost_test(PREFIX ${__PREFIX} TYPE ${e0} SOURCES ${e1} LIBRARIES ${__LIBRARIES})
 
+                    endif()
                 endif()
             endif()
-
         endif()
     endforeach(line)
 
